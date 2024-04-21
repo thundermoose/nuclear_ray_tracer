3f938a22131e693adb0064160b16f1c7701c9020
Tor Djärv <tordjarv@gmail.com>
2024-04-21 10:00:47 -0400

    The camera can generate rays on the GPU

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 compiler := nvcc -c
 linker := nvcc
-
+external_dependencies := cuda
 source_path := ./sources
 release_objects_path := .release_objects
 test_objects_path := .test_objects
@@ -9,8 +9,8 @@ release_dependencies_path := .release_dependencies
 test_dependencies_path := .test_dependencies
 debug_dependencies_path := .debug_dependencies
 
-compiler_flags := -I$(source_path) -I/usr/local/include/blixttest/
-linker_flags := 
+compiler_flags := -I$(source_path) -I/usr/local/include/blixttest/ $(shell pkg-config -cflags $(external_dependencies))
+linker_flags := $(shell pkg-config -libs $(external_dependencies))
 test_linker_flags := -L/usr/local/lib/ -lblixttest
 
 executeable := nuclear_raytracer 
@@ -19,11 +19,7 @@ release_objects := $(sources:$(source_path)/%.cu=$(release_objects_path)/%.o)
 test_objects := $(sources:$(source_path)/%.cu=$(test_objects_path)/%.o)
 debug_objects := $(sources:$(source_path)/%.cu=$(debug_objects_path)/%.o)
 
-release_dependencies := $(sources:$(source_path)/%.cu=$(release_dependencies_path)/%.d)
-test_dependencies := $(sources:$(source_path)/%.cu=$(test_dependencies_path)/%.d)
-debug_dependencies := $(sources:$(source_path)/%.cu=$(debug_dependencies_path)/%.d)
-
-.PRECIOUS: $(release_objects) $(test_objects) $(debug_objects) $(release_dependencies) $(test_dependencies) $(debug_dependencies)
+.PRECIOUS: $(release_objects) $(test_objects) $(debug_objects)
 
 all: release/$(executeable)
 
@@ -51,29 +47,15 @@ clean:
 	rm -rf $(test_objects_path) $(test_dependencies_path)
 	rm -rf $(debug_objects_path) $(debug_dependencies_path)
 
-include $(release_dependencies)
 $(release_objects_path)/%.o: $(source_path)/%.cu
 	mkdir -p $(@D)
 	$(compiler) -o $@ $< $(compiler_flags)
 
-include $(test_dependencies)
 $(test_objects_path)/%.o: $(source_path)/%.cu
 	mkdir -p $(@D)
 	$(compiler) -o $@ $< $(compiler_flags)
 
-include $(debug_dependencies)
 $(debug_objects_path)/%.o: $(source_path)/%.cu
 	mkdir -p $(@D)
 	$(compiler) -o $@ $< $(compiler_flags)
 
-$(release_dependencies_path)/%.d: $(source_path)/%.cu
-	mkdir -p $(@D)
-	$(compiler) -o /dev/null -M -MF $@ -MT $(@:$(release_dependencies_path)/%.d=$(release_objects_path)/%.o) $< $(compiler_flags)
-
-$(test_dependencies_path)/%.d: $(source_path)/%.cu
-	mkdir -p $(@D)
-	$(compiler) -o /dev/null -M -MF $@ -MT $(@:$(test_dependencies_path)/%.d=$(test_objects_path)/%.o) $< $(compiler_flags)
-
-$(debug_dependencies_path)/%.d: $(source_path)/%.cu
-	mkdir -p $(@D)
-	$(compiler) -o /dev/null -M -MF $@ -MT $(@:$(debug_dependencies_path)/%.d=$(debug_objects_path)/%.o) $< $(compiler_flags)
--- sources/camera/camera.cu
+++ sources/camera/camera.cu
@@ -1,21 +1,26 @@
+#include <blixttest/test.hh>
 #include <camera/camera.hh>
 #include <iostream>
 #include <thrust/device_vector.h>
 #include <thrust/generate.h>
 #include <thrust/sequence.h>
 #include <thrust/transform.h>
-#include <blixttest/test.hh>
+#include <vector>
 
-nucray::camera::camera(nucray::vector origin, nucray::vector side,
-                       nucray::vector up, float focal_distance, size_t width,
+nucray::camera::camera(nucray::vector origin,
+                       nucray::vector side,
+                       nucray::vector up,
+                       float focal_distance,
+                       size_t width,
                        size_t height)
     : origin(origin), forward(up.cros(side)), up(up), side(side),
-      focal_distance(focal_distance), width(width), height(height) {}
+      focal_distance(focal_distance), width(width), height(height) {
+}
 
 thrust::device_vector<nucray::ray> nucray::camera::get_rays() {
         thrust::device_vector<size_t> indices(this->width * this->height);
         thrust::sequence(indices.begin(), indices.end());
-        thrust::device_vector<ray> rays;
+        thrust::device_vector<ray> rays(this->width * this->height);
         thrust::transform(indices.begin(), indices.end(), rays.begin(), *this);
         return rays;
 }
@@ -25,19 +30,28 @@ __device__ nucray::ray nucray::camera::operator()(size_t &ray_index) const {
         size_t column = ray_index % this->width;
         nucray::vector lower_left_corner = this->origin - this->side - this->up;
         nucray::vector pos = lower_left_corner +
-                   this->side * (2.0f * (float)column / this->width) +
-                   this->up * (2.0f * (float)row / this->height);
+                             this->side * (2.0f * (float)column / this->width) +
+                             this->up * (2.0f * (float)row / this->height);
         nucray::vector dir = (pos - (origin - forward * focal_distance));
         dir.normalize();
         return nucray::ray(pos, dir);
 }
 
 new_test(creating_rays) {
-        nucray::camera cam(nucray::vector(0,0,0), nucray::vector(1,0,0), nucray::vector(0,1,0),1.0f, 4,4);
+        nucray::camera cam(nucray::vector(0, 0, 0),
+                           nucray::vector(1, 0, 0),
+                           nucray::vector(0, 1, 0),
+                           1.0f,
+                           4,
+                           4);
         auto rays_device = cam.get_rays();
-        thrust::host_vector<nucray::ray> rays_host(rays_device);
-        for (auto &ray: rays_host) {
-                std::cout<<"ray origin:" << ray.start_position.x << " " << ray.start_position.y << " " << ray.start_position.z
-                         <<"ray direction:" << ray.direction.x << " " << ray.direction.y << " " << " " << ray.direction.z << std::endl;
+        std::vector<nucray::ray> rays_host(rays_device.size());
+        thrust::copy(rays_device.begin(), rays_device.end(), rays_host.begin());
+        for (auto &ray : rays_host) {
+                std::cout << "ray origin: " << ray.start_position.x << " "
+                          << ray.start_position.y << " " << ray.start_position.z
+                          << " ray direction:" << ray.direction.x << " "
+                          << ray.direction.y << " "
+                          << " " << ray.direction.z << std::endl;
         }
 }
