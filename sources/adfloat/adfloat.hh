a7f5ce9eb260a55fb277a868fae0d92dcf10b51f
Tor Djärv <tordjarv@gmail.com>
2024-05-12 07:59:30 -0400

    Added some needed numerical methods

--- sources/adfloat/adfloat.hh
+++ sources/adfloat/adfloat.hh
@@ -1,69 +1,54 @@
 #ifndef __ADFLOAT__
 #define __ADFLOAT__
 
+#include <cinttypes>
+#include <cmath>
 namespace nucray {
 struct adfloat {
-        float value, derivative;       
-        __host__ __device__ adfloat operator + (const adfloat other) {
-                adfloat result = {
-                        .value = value + other.value,
-                        .derivative = derivative + other.derivative
-                };
-                return result;
+        float value, derivative;
+        __host__ __device__ adfloat(float value, float derivative)
+            : value(value), derivative(derivative) {
         }
-        __host__ __device__ adfloat operator - (const adfloat other) {
-                adfloat result = {
-                        .value = value - other.value,
-                        .derivative = derivative - other.derivative
-                };
-                return result;
+        __host__ __device__ adfloat operator+(const adfloat other) {
+                return adfloat(value + other.value,
+                               derivative + other.derivative);
         }
-        __host__ __device__ adfloat operator * (const adfloat other) {
-                adfloat result = {
-                        .value = value*other.value,
-                        .derivative = value*other.derivative + derivative*other.value
-                };
-                return result;
+        __host__ __device__ adfloat operator-(const adfloat other) {
+                return adfloat(value - other.value,
+                               derivative - other.derivative);
         }
-        __host__ __device__ adfloat operator / (const adfloat other) {
-                adfloat result = {
-                        .value = value/other.value,
-                        .derivative = (-value*other.derivative + derivative*other.value)/(other.value*other.value)
-                };
-                return result;
+        __host__ __device__ adfloat operator*(const adfloat other) {
+                return adfloat(value * other.value,
+                               value * other.derivative +
+                                   derivative * other.value);
+        }
+        __host__ __device__ adfloat operator/(const adfloat other) {
+                return adfloat(
+                    value / other.value,
+                    (-value * other.derivative + derivative * other.value) /
+                        (other.value * other.value));
         }
         __host__ __device__ adfloat exp() {
-                adfloat result = {
-                        .value = expf(value),
-                        .derivative = derivative*expf(value)
-                };
-                return result;
+                return adfloat(expf(value), derivative * expf(value));
         }
         __host__ __device__ adfloat sin() {
-                adfloat result = {
-                        .value = sinf(value),
-                        .derivative = derivative*cos(value)      
-                };
-                return result;
+                return adfloat(sinf(value), derivative * cosf(value));
         }
         __host__ __device__ adfloat cos() {
-                adfloat result = {
-                        .value = cosf(value),
-                        .derivative = -derivative*sin(value)
-                };
-                return result;
+                return adfloat(cosf(value), -derivative * sinf(value));
         }
         __host__ __device__ adfloat sqrt() {
-                adfloat result = {
-                        .value = sqrtf(value),
-                        .derivative = derivative/sqrtf(value)      
-                };
-                return result;
+                return adfloat(sqrtf(value), derivative / sqrtf(value));
         }
-        __host__ __device__ adfloat powi() {
-                
+        __host__ __device__ adfloat pow(float exponent) {
+                return adfloat(powf(value, exponent), fabs(exponent) < 1e-4 ? 0.0f : exponent*powf(value, exponent-1.0f));
         }
-
+        __host__ __device__ adfloat neg() {
+                return adfloat(-value, -derivative);
+        }
+        __host__ __device__ adfloat asin() {
+                return adfloat(asinf(value),derivative/sqrtf(1-value*value));
+        }
 };
-}
+} // namespace nucray
 #endif
