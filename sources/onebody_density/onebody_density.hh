d11ea566d5e236071fb7e2b1b55bb66bc70578c2
Tor Djärv <tordjarv@gmail.com>
2024-05-12 07:52:12 -0400

    Onebody density for harmonic states

---
+++ sources/onebody_density/onebody_density.hh
@@ -0,0 +1,23 @@
+#ifndef __ONEBODY_DENSITY__
+#define __ONEBODY_DENSITY__
+
+#include <harmonic_state/harmonic_state.hh>
+#include <vector/vector.hh>
+
+namespace nucray {
+class onebody_density {
+        float amplitude;
+        harmonic_state bra, ket;
+
+      public:
+        onebody_density(float amplitude, harmonic_state bra, harmonic_state ket)
+            : amplitude(amplitude), bra(bra), ket(ket) {
+        }
+        __device__ float operator()(vector position) const {
+                return amplitude * abs((conj(bra.wave_function(position)) *
+                       ket.wave_function(position)));
+        }
+};
+} // namespace nucray
+
+#endif
