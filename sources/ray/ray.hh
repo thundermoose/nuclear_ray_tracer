89dafa77399a7e5fae6c748b3b3efb1260a838c3
Tor Djärv <tordjarv@gmail.com>
2024-05-12 07:57:02 -0400

    Method to get a point along a ray

--- sources/ray/ray.hh
+++ sources/ray/ray.hh
@@ -8,9 +8,16 @@ class ray {
       public:
         vector start_position, direction;
 
-        __host__ __device__ ray() {}
-        __host__ __device__ ray(vector start_position, vector direction) :
-                start_position(start_position), direction(direction) {}
+        __host__ __device__ ray() {
+        }
+        __host__ __device__ ray(vector start_position, vector direction)
+            : start_position(start_position), direction(direction) {
+        }
+        __device__ vector get_point(float parameter) const {
+                return vector(start_position.x + direction.x * parameter,
+                              start_position.y + direction.y * parameter,
+                              start_position.z + direction.z * parameter);
+        }
 };
 } // namespace nucray
 
